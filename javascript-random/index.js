b4b810e3bd318cdc45853dea9c4ff87c900200c6
Murad Hallak <murad.hallak.99@hotmail.com>
2024-01-30 19:34:02 -0600

    adding the final message

--- javascript-random/index.js
+++ javascript-random/index.js
@@ -1,21 +1,28 @@
 const firstWord = [
-
+'learning',
+'practicing',
+'applying'
 ];
 
 const secondWord = [
-
+'HTML',
+'Javascript',
+'CSS'
 ];
 
 const thirdWord = [
-
-]
+'amazing',
+'unique',
+'advanced'
+];
 
 const randomSentence = () => {
     const randomOne = firstWord[Math.floor(Math.random() * firstWord.length)];
     const randomTwo = secondWord[Math.floor(Math.random() * secondWord.length)];
     const randomThree = thirdWord[Math.floor(Math.random() * thirdWord.length)];
 
-    const message = `${randomOne} ${randomTwo} ${randomThree}`
+    const message = `${randomOne} ${randomTwo} to your project help you  
+    build a/an ${randomThree} program`
 
     return message;
 }
